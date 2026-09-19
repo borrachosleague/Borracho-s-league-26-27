f898a4faf7c56a8b9c10da43bf78c5ca64536a57
Marco <ventasoppi@libero.it>
2026-09-19 14:08:42 +0200

    Update 19/09/2026

--- apri-sito-online.bat
+++ apri-sito-online.bat
@@ -1,12 +1,14 @@
 @echo off
-echo === AGGIORNAMENTO DATI ===
+echo === 1. AGGIORNAMENTO DATI ===
 python aggiorna.py
 
-echo === PUBLISH SU GITHUB ===
+echo === 2. PUBLISH SU GITHUB ===
 git add .
 git commit -m "Update %date%"
 git push
 
-echo === SITO AGGIORNATO ===
+echo === 3. APERTURA SITO ===
 start https://borrachosleague.github.io/Borracho-s-league-26-27/
+
+echo === FINITO ===
 pause   
